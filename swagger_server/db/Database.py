5db8a1fd1b18e714fd0d03a857ba560457073522
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-11-26 15:12:09 +0100

    Se han modificado los parámetros de acceso a la base de datos remota de Heroku

--- swagger_server/db/Database.py
+++ swagger_server/db/Database.py
@@ -1,22 +1,24 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASS = os.getenv("DATABASE_PASS")
+DATABASE_URL = os.getenv("DATABASE_URL", "ec2-54-217-224-85.eu-west-1.compute.amazonaws.com")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "dbumdeiqr7ebbi")
+DATABASE_USER = os.getenv("DATABASE_USER", "cyzchzivqmaqhz")
+DATABASE_PASS = os.getenv("DATABASE_PASS", "aa8492d1044229569475e0095b565843485352d1390011985d185e3d106cdd7e")
 connection = psycopg2.connect(host=DATABASE_URL, dbname=DATABASE_NAME, user=DATABASE_USER, password=DATABASE_PASS,
-                              port=5432, sslmode="require")
+                              port=5432)
 cursor = connection.cursor()
 print(connection)
 
-postgreSQL_select_Query = "select * from usuarios"
+query = "select * from usuarios"
 
-cursor.execute(postgreSQL_select_Query)
+cursor.execute(query)
 
-print("Selecting rows from mobile table using cursor.fetchall")
-mobile_records = cursor.fetchall()
+print("Selecting rows from usuarios table using cursor.fetchall")
+users = cursor.fetchall()
 
-print(mobile_records)
+print(users)
+
+# close the cursor and the connection pool
 cursor.close()
 connection.close()
