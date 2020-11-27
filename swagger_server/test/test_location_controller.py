c98c0e22917a2bc1b5fa4c73a2c7b942fb33d743
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-11-27 11:50:23 +0100

    Se han mockeado los datos de la BBDD y los accesos

--- swagger_server/test/test_location_controller.py
+++ swagger_server/test/test_location_controller.py
@@ -2,8 +2,9 @@
 
 from __future__ import absolute_import
 
+from unittest import mock
+
 from flask import json
-from six import BytesIO
 
 from swagger_server.models.location import Location  # noqa: E501
 from swagger_server.test import BaseTestCase
@@ -12,42 +13,59 @@ from swagger_server.test import BaseTestCase
 class TestLocationController(BaseTestCase):
     """LocationController integration test stubs"""
 
-    def test_add_location(self):
+    @mock.patch("swagger_server.db.Database.PostgresDB.insert_new_location")
+    def test_add_location(self, mocked_insert_new_location):
         """Test case for add_location
 
         Add a new user location to the system
         """
-        body = Location()
-        mocked_data = {"location": "kitchen"}
+        body = Location("kitchen")
+        mocked_insert_new_location.assert_not_called()
         response = self.client.open(
             '/location',
-            data=json.dumps(mocked_data),
+            data=json.dumps(body),
             method='POST',
             content_type='application/json'
         )
-
+        mocked_insert_new_location.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_historic_location(self):
+    @mock.patch("swagger_server.db.Database.PostgresDB.get_locations")
+    def test_get_historic_location(self, mocked_get_locations):
         """Test case for get_historic_location
 
         Get a historic of locations
         """
+        mocked_get_locations.assert_not_called()
+        mocked_get_locations.return_value = [
+            [1, "mocked_location"],
+            [2, "mocked_location_1"],
+            [3, "mocked_location_2"],
+            [4, "mocked_location_3"],
+        ]
         response = self.client.open(
             '/location',
             method='GET')
+
+        mocked_get_locations.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_location(self):
+    @mock.patch("swagger_server.db.Database.PostgresDB.get_last_location")
+    def test_get_location(self, mocked_get_last_location):
         """Test case for get_location
 
         Get user location
         """
+        mocked_get_last_location.assert_not_called()
+        mocked_get_last_location.return_value = [
+            [1, "mocked_location"]
+        ]
         response = self.client.open(
             '/location/user',
             method='GET')
+        mocked_get_last_location.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
